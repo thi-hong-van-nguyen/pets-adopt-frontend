f359544cdd2f98b8bbc3e2ea76868ca9d0086312
Sanoo Singh <vannguyen141290@gmail.com>
2021-09-22 23:55:42 -0700

    header done

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,24 +3,36 @@ import { Link } from 'react-router-dom';
 
 export default function Header() {
     return (
-        <header style={{display: 'flex', justifyContent: 'space-between'}}>
-            <div>Logo</div>
-            <div className='nav-links'>
-                <ul>
-                    <li><Link to='/'>Home</Link></li>
-                    <li><Link to='/dogs'>Dogs</Link></li>
-                    <li><Link to='/cats'>Cats</Link></li>
-                    <li><Link to='/others'>Others</Link></li>
-                    <li><Link to='/how-it-works'>How it works</Link></li>
-                </ul>
-            </div>
-            <div className='login-logout-menu'>
-                <ul>
-                    <li><Link to='/login'>Log in</Link></li>
-                    <li><Link to='/signup'>Sign up</Link></li>
-                    <li><Link to='/logout'>Log out</Link></li>
-                </ul>
-                <span>Logged in as ________</span>
+        <header>
+            <div className='header'>
+                <div className='logo-slogan-container'>
+                    <div><img id='logo' src='logopaw.PNG' alt='paw' /></div>
+
+                    <div id='slogan'>
+                        <span id='slogan-1'>From</span>
+                        <span id='slogan-2'>One</span>
+                        <span id='slogan-3'>Home</span>
+                        <span id='slogan-4'>To</span>
+                        <span id='slogan-5'>Another</span>
+                    </div>
+                </div>
+                <div className='nav-links'>
+                    <ul>
+                        <li><Link to='/'>Home</Link></li>
+                        <li><Link to='/dogs'>Dogs</Link></li>
+                        <li><Link to='/cats'>Cats</Link></li>
+                        <li><Link to='/others'>Others</Link></li>
+                        <li><Link to='/how-it-works'>How it works</Link></li>
+                    </ul>
+                </div>
+                <div className='login-logout-menu'>
+                    <ul>
+                        <li id='login'><Link to='/login'>Log in</Link></li>
+                        <li id='signup'><Link to='/signup'>Sign up</Link></li>
+                       {/* <li id='logout'><Link to='/logout'>Log out</Link></li>
+                       <li id='login-as'>Logged in as ________</li> */}
+                    </ul>
+                </div>
             </div>
         </header>
     )
