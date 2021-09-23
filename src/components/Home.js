6571b563840f8237eefeeaeb43cb5910fe38126d
Sanoo Singh <vannguyen141290@gmail.com>
2021-09-23 01:24:19 -0700

    working on Main

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,28 @@ import React from 'react';
 
 export default function Home() {
     return (
-        <div>
-this is home
+        <div id='main'>
+            <div className='get-started-container'>
+                <img src='topcontent.PNG' alt='pet'/>
+                <div className='get-started'>
+                    <h2>Get Personalized Pet Matches</h2>
+                    <div>Take a quick Quiz to find your perfect matched pet on AdoptMe</div>
+                    <button>GET STARTED</button>
+                </div>
+            </div>
+            <div className='search-bar'>
+
+            </div>
+            <div className='pet-images-container'>
+
+            </div>
+
+            <div className='learn-more'>
+
+            </div>
+            <div className='articles-container'>
+
+            </div>
         </div>
     )
 }
