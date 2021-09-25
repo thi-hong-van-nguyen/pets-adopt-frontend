5e9884c2e00547574ea5e5353348481144252bf6
Sanoo Singh <vannguyen141290@gmail.com>
2021-09-24 20:44:41 -0700

    added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,16 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
 import dummyData from '../services/dummyData';
 import { fetchCats, fetchDogs } from '../actions';
 
 function Pet(props) {
-    const { name, url} = props.pet;
+    const { name, url } = props.pet;
     return (
         <div className='card'>
             <div><img src={url} alt='pet' /></div>
-            <div>{name}</div>
+            <div className='card-pet-name'>{name}</div>
         </div>
     )
 }
@@ -17,6 +18,7 @@ function Pet(props) {
 function Home(props) {
     const { fetchCats, fetchDogs } = props;
     const { cats, dogs } = props.data;
+    const { push } = useHistory();
 
     useEffect(() => {
         fetchDogs(dummyData.dogs);
@@ -36,6 +38,7 @@ function Home(props) {
                     </div>
                 </div>
             </div>
+
             <div className='pet-care-container'>
                 <div className='pet-care'>
                     <a id='pet-care-1' href='#top'>
@@ -55,6 +58,7 @@ function Home(props) {
                     </a>
                 </div>
             </div>
+
             <div className='pet-images-container'>
                 <div id='pets-list'>
                     <div>
@@ -64,10 +68,10 @@ function Home(props) {
                         {cats.filter((cat, index) => index < 2).map(item => (<Pet key={item.id} pet={item} />))}
                         {dogs.filter((dog, index) => index < 2).map(item => (<Pet key={item.id} pet={item} />))}
                         <div className='card card-5'>
-                            <div style={{color: 'white'}}>
+                            <div style={{ color: 'white' }}>
                                 {cats.length + dogs.length} more pets available on Adopt Me
                             </div>
-                            <div style={{color: 'white'}}>
+                            <div style={{ color: 'white' }}>
                                 See More
                             </div>
                         </div>
@@ -76,11 +80,61 @@ function Home(props) {
                 </div>
             </div>
 
-            <div className='learn-more'>
+            <div className='planning-adoption-container'>
+                <div className='planning-adoption'>
+                    <h2>Planning to Adopt a Pet?</h2>
+                    <div className='planning-adoption-cards'>
+                        <div className='planning-adoption-card planning-adoption-card-1'>
+                            <h2>Checklist for New Adopters</h2>
+                            <p>Help make the transition, as smooth as possible</p>
+                            <div>
+                                <button>LEARN MORE</button>
+                            </div>
+                        </div>
+                        <div className='planning-adoption-card planning-adoption-card-2'>
+                            <h2>Resources</h2>
+                            <p>Get the latest on adoption processes, <br/>learn how local shelters and rescue groups are adapting and find out what you can do to help dogs and cats in need right now.</p>
+                            <div>
+                                <button>LEARN MORE</button>
+                            </div>
+                        </div>
+                        <div className='planning-adoption-card planning-adoption-card-3'>
+                            <h2>Pet Adoption FAQs</h2>
+                            <p>Get answers to questions you haven't thought of.</p>
+                            <div>
+                                <button>LEARN MORE</button>
+                            </div>
+                        </div>
+                    </div>
 
+                </div>
             </div>
-            <div className='articles-container'>
 
+            <div className='articles-container'>
+                <div className='articles'>
+                    <div className='article' onClick={() => push('/')}>
+                        <img className='article-img-1' src='https://www.litter-robot.com/blog/wp-content/uploads/2016/01/girl-and-cat-2-1080-510x339.jpg' alt='cat and baby' />
+                        <img className='article-img-2' src='https://www.litter-robot.com/blog/wp-content/uploads/2016/01/girl-and-cat-2-1080-510x339.jpg' alt='cat and baby' />
+                        <div className='article-text'>
+                            <div>
+                                <h3>Cat Adoption Articles</h3>
+                                <p>Helpful insights of what to expect.</p>
+                            </div>
+                            <div>READ MORE</div>
+                        </div>
+                    </div>
+                    <div className='article' onClick={() => push('/')}>
+                        <img className='article-img-1' src='https://cdn-prod.medicalnewstoday.com/content/images/articles/292/292829/woman-and-her-dog.jpg' alt='dog and human' />
+                        <img className='article-img-2' src='https://cdn-prod.medicalnewstoday.com/content/images/articles/292/292829/woman-and-her-dog.jpg' alt='dog and human' />
+                        <div className='article-text'>
+                            <div>
+                                <h3>Dog Adoption Articles</h3>
+                                <p>Learn more abour caring for your new dog.</p>
+                            </div>
+                            <div>READ MORE</div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
